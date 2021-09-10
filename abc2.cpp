a8fe7894f55cfeed90ef44abf04fd2865b6a43ed
Nimanita <45235857+Nimanita@users.noreply.github.com>
2021-09-10 21:08:07 +0530

    Update abc2.cpp

--- abc2.cpp
+++ abc2.cpp
@@ -5,4 +5,8 @@ const username = "asfweferg.com";
 const phoneNumber = "rgekjrng kjrgnjke";
 const salary ="serger";
 const stripeAcct = 12123;
+salary = 100
+salaryEmp = 100
+email = “hit@mail.com”
+first_name()
 const username = "asfweferg.com";
