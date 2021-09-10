567350949b66105cc48dc45a0d15bbe22dda8e4d
Nimanita <45235857+Nimanita@users.noreply.github.com>
2021-09-10 20:47:38 +0530

    Create abc2.cpp

---
+++ abc2.cpp
@@ -0,0 +1,8 @@
+const phoneNumber = "rgekjrng kjrgnjke";
+const salary ="serger";
+const stripeAcct = 12123;
+const username = "asfweferg.com";
+const phoneNumber = "rgekjrng kjrgnjke";
+const salary ="serger";
+const stripeAcct = 12123;
+const username = "asfweferg.com";
