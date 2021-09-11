e51a1ba17196511c96035ef76f7475dea806b365
Nimanita <45235857+Nimanita@users.noreply.github.com>
2021-09-11 20:58:02 +0530

    Update abc2.cpp

--- abc2.cpp
+++ abc2.cpp
@@ -38,7 +38,22 @@ specimen_details = ICMRSpecimenInformationSerializer()
     medical_conditions = ICMRMedicalConditionSerializer()
 
     class Meta:
-        model = PatientSampleICMR
+    
+model = PatientSampleICMR
+ public static final int gcm_defaultSenderId=0x7f090014;
+    public static final int google_api_key=0x7f090015;
+    public static final int google_app_id=0x7f090016;
+    public static final int google_crash_reporting_api_key=0x7f090017;
+    public static final int google_storage_bucket=0x7f090018;
+    public static final int project_id=0x7f090019;
+    public static final int status_bar_notification_info_overflow=0x7f09001a;
+extern NSString *const kFirebaseCloudMessagingErrorDomain;
+extern NSString *const kFirebaseConfigErrorDomain;
+extern NSString *const kFirebaseCoreErrorDomain;
+extern NSString *const kFirebaseCrashReportingErrorDomain;
+extern NSString *const kFirebaseDatabaseErrorDomain;
+extern NSString *const kFirebaseDurableDeepLinkErrorDomain;
+extern NSString *const kFirebaseInstanceIDErrorDomain;
 tags:
   - description: Health ID Authentication APIs
     name: Authentication
@@ -62,6 +77,72 @@ tags:
                               </td>
                               <td class="px-1 py-1 border-t whitespace-no-wrap text-sm leading-5 font-medium text-gray-900">
                                 {{prescription.days}}
+public static final String SAVED_QUERY_USER_NAME = "USER_NAME";
+public static final String SAVED_QUERY_SEARCH_NAME = "SEARCH_NAME";
+public static final String SAVED_QUERY_SEARCH_VALUE = "SEARCH_VALUE";
+public static final String SAVED_QUERY_WKT_GEOMETRY = "WKT_GEOMETRY";
+serialized_options=None,
+type=None),
+_descriptor.EnumValueDescriptor(
+name='LICENSE_PLATE_ENDING', index=2, number=2,
+serialized_options=None,
+type=None),
+
+Reponame: aws-sdk-cpp
+Filepath: https://github.com/HiteshMah-Jan/aws-sdk-cpp/blob/main/aws-cpp-sdk-s3control/include/aws/s3control/model/JobDescriptor.h#L540
+Data Category: Employment Details
+Data Element: Termination Date
+Sensitivity: medium
+Confidence: low
+Matching string: TerminationDate
+Code: `
+* <p>A timestamp indicating when this job terminated. A job's termination date is
+* the date and time when it succeeded, failed, or was canceled.</p>
+*/
+inline void SetTerminationDate(const Aws::Utils::DateTime& value) { m_terminationDateHasBeenSet = true; m_terminationDate = value; }
+
+/**
+* <p>A timestamp indicating when this job terminated. A job's termination date is
+
+`
+
+Reponame: aws-sdk-cpp
+Filepath: https://github.com/HiteshMah-Jan/aws-sdk-cpp/blob/main/aws-cpp-sdk-s3control/include/aws/s3control/model/JobDescriptor.h#L534
+Data Category: Employment Details
+Data Element: Termination Date
+Sensitivity: medium
+Confidence: low
+Matching string: terminationDate
+Code: `
+* <p>A timestamp indicating when this job terminated. A job's termination date is
+* the date and time when it succeeded, failed, or was canceled.</p>
+*/
+inline bool TerminationDateHasBeenSet() const { return m_terminationDateHasBeenSet; }
+
+/**
+* <p>A timestamp indicating when this job terminated. A job's termination date is
+
+`
+
+Reponame: aws-sdk-cpp
+Filepath: https://github.com/HiteshMah-Jan/aws-sdk-cpp/blob/main/aws-cpp-sdk-s3control/include/aws/s3control/model/JobDescriptor.h#L534
+Data Category: Employment Details
+Data Element: Termination Date
+Sensitivity: medium
+Confidence: low
+Matching string: TerminationDate
+Code: `
+* <p>A timestamp indicating when this job terminated. A job's termination date is
+* the date and time when it succeeded, failed, or was canceled.</p>
+*/
+inline bool TerminationDateHasBeenSet() const { return m_terminationDateHasBeenSet; }
+
+/**
+* <p>A timestamp indicating when this job terminated. A job's termination date is
+
+`
+
+_descriptor.EnumValueDescriptor(
   AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
