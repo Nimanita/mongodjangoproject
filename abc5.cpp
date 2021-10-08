79e218b91947072a0438a0348735ca6bd0b48e3f
Nimanita <45235857+Nimanita@users.noreply.github.com>
2021-10-08 21:51:14 +0530

    Update abc5.cpp

--- abc5.cpp
+++ abc5.cpp
@@ -2,4 +2,14 @@
 user name = firstname()
 schooldetails
 account Id = user.id("3838")
+          {
+        firstName: 'Peter',
+        lastName: 'Parker',
+        accountId: twitter_channel.inbox.account_id,
+        profilePic: 'https://via.placeholder.com/250x250.png'
+          useremail = "abc@gmail.com"
+          
+      }
+    )
+   user.email()
 #f
