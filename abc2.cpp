a7698d7bfeb68e2a8fa3335e29cecd3692008ee8
Nimanita <45235857+Nimanita@users.noreply.github.com>
2021-10-09 15:43:06 +0530

    Update abc2.cpp

--- abc2.cpp
+++ abc2.cpp
@@ -23,7 +23,7 @@ account Id = user.id("3838")
         profilePic: 'https://via.placeholder.com/250x250.png'
           useremail = "abc@gmail.com"
           
-      }
+      }'
     )
               middle name = user.middlename
    user.email()
