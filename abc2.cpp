2802cf25fcf739fd2cd13a997201fbf9a1ea4c34
Nimanita <45235857+Nimanita@users.noreply.github.com>
2021-10-09 12:18:27 +0530

    Update abc2.cpp

--- abc2.cpp
+++ abc2.cpp
@@ -21,6 +21,15 @@ account Id = user.id("3838")
                     userActivityLvl.setText(getActivityLvlShortForm(object.getString("activityLvl")));
 
 #fh
+
+render() {},
+      mixins: [inboxMixin],
+      data() {
+        return { inbox: { channel_type: 'Channel::TwitterProfile' } };
+      },
+    };
+    const wrapper = shallowMount(Component);
+
 return self._financial_statement_data('income', 'incomeStatementHistory', 'costOfRevenue', 'annual')
 
     def get_income_before_tax(self):
