2f40a92423d32acc09b6eeaeae4ca7c13eff4142
Nimanita <45235857+Nimanita@users.noreply.github.com>
2021-10-09 11:02:07 +0530

    Update abc5.cpp

--- abc5.cpp
+++ abc5.cpp
@@ -4,6 +4,7 @@ schooldetails
 account Id = user.id("3838")
           {
         firstName: 'Peter',
+          
         lastName: 'Parker',
         accountId: twitter_channel.inbox.account_id,
         profilePic: 'https://via.placeholder.com/250x250.png'
