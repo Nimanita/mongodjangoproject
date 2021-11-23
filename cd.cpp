87abad58adbfaf08fc853107a697f3f9faff9034
Nimanita <45235857+Nimanita@users.noreply.github.com>
2021-11-23 16:09:50 +0530

    Update cd.cpp

--- cd.cpp
+++ cd.cpp
@@ -4,6 +4,20 @@
 
 
 
+instagram.com
+   $instagram = \InstagramScraper\Instagram::withCredentials(new \GuzzleHttp\Client(), 'username', 'password', new Psr16Adapter('Files'));
+$instagram->login();
+
+$media = $instagram->getMediaByUrl('https://www.instagram.com/p/BQ0lhTeAYo5');
+echo "Media info:\n";
+printMediaInfo($media);
+$instagram = \InstagramScraper\Instagram::withCredentials(new \GuzzleHttp\Client(), 'username', 'password', new Psr16Adapter('Files'));
+$instagram->login();
+
+$media = $instagram->getMediaByUrl('https://www.instagram.com/p/BHaRdodBouH');
+echo "Media info:\n";
+echo "Id: {$media->getId()}\n";
+echo "Shortcode: {$media->getShortCode()}\n";
 
 
 
